9ee7b684f14301864a46936ae8da1406986c7ec3
Fredy Daniel Flores Lemus <fredyflemus@gmail.com>
2025-01-12 10:23:20 -0600

    put data into dynamodb with lambda

--- src/handlers/cy-store-data.mjs
+++ src/handlers/cy-store-data.mjs
@@ -1,5 +1,24 @@
-export const cyStoreDataHandler = (event, context, callback) => {
-    console.log(event)
-    const age = event.age
-    callback(null, age * 2)
+import {DynamoDBClient} from 'aws-sdk/client-dynamodb'
+import { 
+    DynamoDBDocumentClient, PutCommand
+  } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({region: 'us-east-1'});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const cyStoreDataHandler = async (event, context, callback) => {
+  const params = {
+    Item: {UserId: "dasasdsa", Age: 27, Height: 72, Income: 2500},
+    TableName: "compare-yourself-table"
+  }
+
+  try{
+    const data = await docClient.send(new PutCommand(params));
+    console.log("Success - item added or updated", data);
+    callback(null, data)
+  }catch(error){
+    console.log("Error", err.stack);
+    callback(e)
+  }
 }
