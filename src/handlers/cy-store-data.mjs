8936840446035014b1b5cc6309189d8564c0f9db
Fredy Daniel Flores Lemus <fredyflemus@gmail.com>
2025-01-12 21:06:53 -0600

    use env variables

--- src/handlers/cy-store-data.mjs
+++ src/handlers/cy-store-data.mjs
@@ -7,10 +7,12 @@ const client = new DynamoDBClient({region: 'us-east-1'});
 
 const docClient = DynamoDBDocumentClient.from(client);
 
+const tableName = process.env.DYNAMO_TABLE_NAME;
+
 export const cyStoreDataHandler = async (event, context, callback) => {
   const params = {
     Item: {UserId: "user_"+Math.random(), Age: event.age, Height: event.height, Income: event.income},
-    TableName: "compare-yourself-table"
+    TableName: tableName
   }
 
   try{
