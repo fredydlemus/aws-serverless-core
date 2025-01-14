09cf20bcc049781b1d7a56d0634705c689b2c331
Fredy Daniel Flores Lemus <fredyflemus@gmail.com>
2025-01-13 21:55:15 -0600

    integrate dynamodb

--- src/handlers/cy-get-data.mjs
+++ src/handlers/cy-get-data.mjs
@@ -1,11 +1,35 @@
-export const cyGetDataHandler = (event, context, callback) => {
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import {
+    DynamoDBDocumentClient, ScanCommand
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ region: 'us-east-1' });
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.DYNAMO_TABLE_NAME;
+
+export const cyGetDataHandler = async (event, context, callback) => {
     console.log(event)
     const type = event.type
-    if(type === 'all'){
-        callback(null, 'all the data')
-    }else if (type === 'single'){
+    if (type === 'all') {
+        try {
+
+            const params = {
+                TableName: tableName
+            }
+
+            const data = await docClient.send(new ScanCommand(params))
+            console.log(data)
+
+            callback(null, data)
+        } catch (error) {
+            console.log("Error", error.stack);
+            callback(error)
+        }
+    } else if (type === 'single') {
         callback(null, 'Just my data')
-    }else{
+    } else {
         callback(null, 'Hello from lambda!')
     }
 }
