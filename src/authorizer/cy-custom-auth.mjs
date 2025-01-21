d27f6e3a32ae84cbf935883349dd3e753d46cdef
Fredy Daniel Flores Lemus <fredyflemus@gmail.com>
2025-01-21 09:44:57 -0600

    add custom auth

---
+++ src/authorizer/cy-custom-auth.mjs
@@ -0,0 +1,46 @@
+export const cyCustomAuthHandler = async (event, context, callback)  => {
+    const token = event.authorizationToken;
+    //Use token
+    if(token === 'allow'){
+        const policy = genPolicy('allow', event.methodArn);
+        const principalId = 'asdfgh';
+        const context = {
+            simpleAuth: true
+        }
+
+        const response = {
+            principalId,
+            policyDocument: policy,
+            context
+        };
+
+        callback(null, response)
+    }else if(token === 'deny'){
+        const policy = genPolicy('deny', event.methodArn);
+        const principalId = 'asdfgh';
+        const context = {
+            simpleAuth: true
+        }
+
+        const response = {
+            principalId,
+            policyDocument: policy,
+            context
+        };
+
+        callback(null, response)
+    }else{
+        callback('Unauthorized')
+    }
+}
+
+function genPolicy(effect, resource){
+    const policy = {}
+    policy.Version = '2012-10-17';
+    policy.Statement = [];
+    const stmt = {}
+    stmt.Action = 'execute-api:Invoke'
+    stmt.Effect = effect;
+    stmt.Resource = resource;
+    policy.Statement.push(stmt)
+}
